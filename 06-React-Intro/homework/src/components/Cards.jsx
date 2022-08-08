0874e090348be3bbfa6d505bd7d6983aa2a74598
Roger Perez <roger.perezcol@gmail.com>
2022-08-08 16:15:29 -0300

    react

--- 06-React-Intro/homework/src/components/Cards.jsx
+++ 06-React-Intro/homework/src/components/Cards.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import Card from './Card';
 
 export default function Cards(props) {
-  // acá va tu código
-  // tip, podés usar un map
-  return <div>Cards Component</div>
+  // acá va tu código tip, podés usar un map
+  return (
+    <div>
+      {
+      props
+        .cities
+        .map((city, i) => (
+        <Card
+        name={city.name}
+        max={city.main.temp_max}
+        min={city.main.temp_min}
+        img={city.weather[0].icon}
+        onClose={() => alert(city.name)}
+        key={i}
+          />
+          ))
+}
+    </div>
+  )
 };
--- 06-React-Intro/homework/src/components/SearchBar.jsx
+++ 06-React-Intro/homework/src/components/SearchBar.jsx
@@ -2,5 +2,19 @@ import React from 'react';
 
 export default function SearchBar(props) {
   // acá va tu código
-  return <div>Search Bar Component</div>
+  return (
+    <div>
+      <div class="form-inline md-form mr-auto mb-4">
+        <input
+          class="form-control mr-sm-2"
+          type="text"
+          placeholder="Search"
+          aria-label="Search"
+        />
+        <button onClick={props.onSearch} class="btn aqua-gradient btn-rounded btn-sm my-0" >
+          Search
+        </button>
+      </div>
+    </div>
+  );
 };
